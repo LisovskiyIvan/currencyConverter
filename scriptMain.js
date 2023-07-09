ea1fceb0a84d378a049e56af99a747bde7d5ee73
LisovskiyIvan <136194732+LisovskiyIvan@users.noreply.github.com>
2023-07-09 14:09:16 +0300

    Add files via upload

---
+++ scriptMain.js
@@ -0,0 +1,44 @@
+
+    const swapper = document.getElementById('swapper');
+    const inputNumber = document.getElementById('currInput');
+    const outputNumber = document.getElementById('currOutput');
+    const inputCurrencySelector = document.getElementById('inputCurrencySelector');
+    const outputCurrencySelector = document.getElementById('outputCurrencySelector');
+    
+    swapper.addEventListener('click', swap);
+    
+    inputNumber.addEventListener( 'input', converter);
+    inputCurrencySelector.addEventListener('change', converter);
+    outputCurrencySelector.addEventListener('change', converter);
+    
+    
+    async function converter() {
+         
+        let inputCurrency = inputCurrencySelector.value;
+        let outputCurrency = outputCurrencySelector.value;
+        let number = Number(inputNumber.value);
+        let data = {
+            firstCurrency: inputCurrency,
+            amount: number,
+            secondCurrency: outputCurrency
+        };
+        
+         await fetch('http://localhost:8081', {
+            method: 'POST',
+            'Access-Control-Allow-Origin': '*',
+            mode: 'cors',
+            body: JSON.stringify(data)
+        })
+        .then ((res) => res.json())
+        .then ((output) => outputNumber.value = Number(output.Value).toFixed(2))
+         
+    };
+    
+    
+    function swap () {
+        temp = inputCurrencySelector.value;
+        inputCurrencySelector.value = outputCurrencySelector.value;
+        outputCurrencySelector.value = temp;
+        converter(); 
+    }
+    
